bbb213e1f1eadab7a64aaf40cc2243b9151a6f25
Leo P <junk@slact.net>
2019-05-29 16:37:28 -0400

    prettier log, uglier code (it's temporary though so it's ok)

--- src/include/shuttlesock/log.h
+++ src/include/shuttlesock/log.h
@@ -3,9 +3,16 @@
 #include <stdio.h>
 
 #define shuso_log(ctx, ...) do { \
-  printf("[%d %s] ", getpid(), (ctx->procnum == SHUTTLESOCK_NOPROCESS ? "   none" : (ctx->procnum == SHUTTLESOCK_MASTER ? " master" : (ctx->procnum == SHUTTLESOCK_MANAGER ? "manager" : " worker")))); \
-  printf(__VA_ARGS__); \
-  printf("\n"); \
+  char ___log[2048]; \
+  char *___cur = ___log; \
+  ___cur += sprintf(___cur, "[%d %s", getpid(), (ctx->procnum == SHUTTLESOCK_NOPROCESS ? "none     " : (ctx->procnum == SHUTTLESOCK_MASTER ? "master   " : (ctx->procnum == SHUTTLESOCK_MANAGER ? "manager  " : "worker ")))); \
+  if(ctx->procnum >= SHUTTLESOCK_WORKER) { \
+    ___cur += sprintf(___cur, "%i ", ctx->procnum); \
+  } \
+  ___cur += sprintf(___cur, "] "); \
+  ___cur += sprintf(___cur, __VA_ARGS__); \
+  ___cur += sprintf(___cur, "\n"); \
+  printf(___log); \
 } while(0)
 
 #define shuso_loop_log(loop, ...) \
