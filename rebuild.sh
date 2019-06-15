a079362fab0a8d0bace111aeaf9703866fcb01b6
Leo P <junk@slact.net>
2019-06-15 00:31:34 -0400

    neato rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -4,10 +4,10 @@ MY_PATH="`( cd \"$MY_PATH\" && pwd )`"
 _src_dir=${MY_PATH}/src
 
 ALL_OFF="\e[1;0m"
-BLUE="${BOLD}\e[1;34m"
-GREEN="${BOLD}\e[1;32m"
-RED="${BOLD}\e[1;31m"
-YELLOW="${BOLD}\e[1;33m"
+BLUE="\e[1;34m"
+GREEN="\e[1;32m"
+RED="\e[1;31m"
+YELLOW="\e[1;33m"
 
 OPTS=()
 compiler=""
@@ -60,11 +60,19 @@ for opt in $*; do
       clang_analyze=1
       disable_ccache=1
       ;;
+    valgrind)
+      valgrind=1
+      ;;
+    stalloc_track_space)
+      stalloc_track_space=1
+      ;;
+    sanitize)
+      sanitize=1
+      ;;
     verbose|-v|v)
       verbose="-v"
       ;;
     coverage)
-      compiler=gcc
       run_test=1
       build_type=DebugCoverage
       ;;
@@ -87,48 +95,64 @@ for opt in $*; do
     test|runtest)
       run_test=1
       ;;
-    *)
+    -*)
       OPTS+=( "$opt" )
+      ;;
+    *)
+      echo "${RED}>> invalid option ${ALL_OFF}$opt${ALL_OFF}" 1>&2
+      invalid_options=1
       ;;
   esac
 done
 
+if [[ -n $invalid_options ]]; then
+  exit 1
+fi
+
 if [[ -n $clean ]]; then
+  echo "${YELLOW}>> rm -Rf $build_dir"
   rm -Rf $build_dir
 fi
 
+OPTS+=( "-DCMAKE_BUILD_TYPE=$build_type" )
 if [[ -n $compiler ]]; then
   OPTS+=( "-DCMAKE_C_COMPILER=$compiler" )
-fi
-if [[ -n $build_type ]]; then
-  OPTS+=( "-DCMAKE_BUILD_TYPE=$build_type" )
 fi
 if [[ -n $optimize_level ]]; then
   OPTS+=( "-DOPTIMIZE_LEVEL=$optimize_level" )
 fi
-if [[ -n $disable_ccache ]]; then
-  OPTS+=( "-DDISABLE_CCACHE=1" )
-fi
+
+OPTS+=( "-DSHUTTLESOCK_STALLOC_TRACK_SPACE=${stalloc_track_space}" )
+OPTS+=( "-DSHUTTLESOCK_VALGRIND=${valgrind}" )
+OPTS+=( "-DSHUTTLESOCK_SANITIZE=${sanitize}" )
+OPTS+=( "-DDISABLE_CCACHE=${disable_ccache}" )
 
 if [[ -n $clang_analyze ]]; then
   ANALYZE=("scan-build")
   ANALYZE+=($ANALYZE_FLAGS)
+  ANALYZE+=("")
 fi
 
 TRAPINT() {
   if [[ -n $scan_view_pid ]]; then
-    kill -s INT $scan_view_pid
+    kill -s TERM $scan_view_pid
   else
     exit 1
   fi
 }
 
-echo $BLUE $ANALYZE $YELLOW cmake $GREEN $OPTS $YELLOW -B$build_dir $ALL_OFF
+print -n "\n${YELLOW}>> ${BLUE}${ANALYZE}${YELLOW}cmake${ALL_OFF}"
+
+for opt in $OPTS ; do
+  print -n " ${GREEN}\\\\\n $opt${ALL_OFF}"
+done
+print -n " ${GREEN}\\\\\n ${YELLOW}-B$build_dir $ALL_OFF\n\n"
+
 $ANALYZE cmake $OPTS -B$build_dir
 if ! [ $? -eq 0 ]; then;
   exit 1
 fi
-echo $BLUE $ANALYZE $YELLOW cmake $YELLOW --build $build_dir $verbose $ALL_OFF
+echo "\n$YELLOW>> ${BLUE}${ANALYZE}${YELLOW}cmake ${YELLOW}--build $build_dir $verbose $ALL_OFF\n"
 
 if [[ -n $clang_analyze ]]; then
   $ANALYZE cmake --build $build_dir $verbose &
@@ -145,21 +169,26 @@ fi
 
 if [[ -n $run_test ]]; then
   pushd $build_dir
+  echo "\n${GREEN}>> Running tests...${ALL_OFF}"
   ./shuso_test
-  popd $build_dir
+  if ! [ $? -eq 0 ]; then;
+    echo "\n${RED}>> tests failed.${ALL_OFF}"
+    exit 1
+  fi
+  popd
 fi
 
 if [[ $build_type == "DebugCoverage" ]]; then
   pushd $build_dir
-  ./shuso_test
   #ls CMakeFiles/shuttlesock.dir/src -alh
   #ls ../src -alh
+  echo "\n${GREEN}>> Preparing coverage results...${ALL_OFF}"
   if [[ $compiler == "gcc" ]]; then
     mkdir coverage-report 2>/dev/null
-    gcovr --root ../src -v --html-details -o coverage-report/index.html ./
+    gcovr --root ../src --html-details -o coverage-report/index.html ./
   elif [[ $compiler == "clang" ]]; then
     llvm-profdata merge -sparse *.profraw -o .profdata
-    llvm-cov show -format="html" -output-dir="coverage-report" -instr-profile=".profdata" "libshuttlesock.so" -object "shuso_test"
+    llvm-cov show -format="html" -output-dir="coverage-report" -instr-profile=".profdata"  -ignore-filename-regex="test/.*" -ignore-filename-regex="lib/.*" "libshuttlesock.so" -object "shuso_test"
   fi
 
   xdg-open ./coverage-report/index.html
