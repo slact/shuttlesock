e5c2b8291530406f3e240e69192da90ea900b66d
Leo P <junk@slact.net>
2019-06-15 00:36:37 -0400

    test for the __typeof__ keyword for good measure

---
+++ cmake/TestTypeofKeyword.cmake
@@ -0,0 +1,26 @@
+include(CheckCSourceCompiles)
+include(CMakePushCheckState)
+
+function(test_typeof_keyword typeof_keyword_var)
+  if(DEFINED CACHE{${typeof_keyword_var}})
+    return()
+  endif()
+  message(STATUS "Check if __typeof__() exist")
+  cmake_push_check_state(RESET)
+  set(CMAKE_REQUIRED_QUIET 1)
+  set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} -Werror -std=c11")
+  check_c_source_compiles("
+    int main(void) {
+      int *foo;
+      __typeof__(*foo) bar;
+      return 0;
+    }
+  " "${typeof_keyword_var}")
+  cmake_reset_check_state()
+  
+  if(${typeof_keyword_var})
+    message(STATUS "Check if __typeof__() exist - yes")
+  else()
+    message(STATUS "Check if __typeof__() exist - no")
+  endif()
+endfunction()
