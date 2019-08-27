7c053f5fc2e83b8958f0e7f8d6fa5d0b68b05267
Leo P <junk@slact.net>
2019-08-27 18:14:28 -0400

    nicer parser

--- src/lua/config_parser.lua
+++ src/lua/config_parser.lua
@@ -15,6 +15,10 @@ local function countSlashesReverse(str, start, cur)
   return count
 end
 
+local function indent(lvl)
+  return ("  "):rep(lvl)
+end
+
 function Parser.new(string)
   local self = {
     str = string,
@@ -37,37 +41,59 @@ function Parser.loadFile(path)
   local str = f:read("*all")
   f:close()
   local parser = Parser.new(str)
-  return parser:parse()
+  parser:parse()
+  return parser
 end
 
 function Parser.loadString(string, name)
   local parser = Parser.new(string)
-  return parser:parse()
+  parser:parse()
+  return parser
 end
 
 local chunk_mt = {__index = {
-  type = "chunk",
+  addDirective = function(self, directive)
+    assert(self.type == "block")
+    table.insert(self.directives, directive)
+    table.insert(self.tokens, directive)
+    return true
+  end,
+  
   setBlock = function(self, block)
+    assert(self.type == "directive")
     assert(not self.block)
     self.block = block
     return true
   end,
   
   addValue = function(self, val)
+    assert(self.type == "directive")
     assert(type(val) == "table")
-    if not self.position.value_first then
-      self.position.value_first = val.position.first
+    if not self.position.values_first then
+      self.position.values_first = val.position.first
     end
-    self.position.value_last = val.position.last
+    self.position.values_last = val.position.last
     table.insert(self.values, val)
+    table.insert(self.tokens, val)
     return true
-  end
-}}
-local block_mt = {__index = {
-  type = "block",
-  addChunk = function(self, chunk)
-    table.insert(self.chunks, chunk)
+  end,
+  
+  addNewline = function(self, pos)
+    table.insert(self.tokens, {type="newline", position = pos, value = ""})
     return true
+  end,
+  addComment = function(self, comment, pos)
+    assert(type(comment) == "string")
+    assert(type(pos) == "number")
+    local cmt = {type="comment", position = pos, value = comment}
+    table.insert(self.comments, cmt)
+    table.insert(self.tokens, cmt)
+    return true
+  end,
+  addSemicolon = function(self, pos)
+    assert(self.type == "directive")
+    local semi = {type="semicolon", position = pos}
+    table.insert(self.tokens, semi)
   end
 }}
 
@@ -75,53 +101,57 @@ parser_mt = {__index = {
   
   error = function(self, err)
     if err then
-      self.errmsg = err
-      error(err)
+      local line, column = self:location()
+      self.errmsg = ("%s in %s:%d:%d"):format(err, "config", line, column)
+      error(self.errmsg)
       return nil, err
     else
       return self.errmsg
     end
   end,
   
-  inChunk = function(self)
-    return self.top.type == "chunk"
+  inDirective = function(self)
+    return self.top.type == "directive"
   end,
   inBlock = function(self)
     return self.top.type == "block"
   end,
   
-  pushChunk = function(self, chunk_name)
+  pushDirective = function(self, directive_name)
     assert(self:inBlock())
-    local chunk = {
-      name = chunk_name,
+    local directive = {
+      type = "directive",
+      name = directive_name,
       position = {
         first = self.cur,
         last = self.cur,
-        value_first = self.cur,
-        value_last = self.cur
+        values_first = nil,
+        values_last = nil
       },
       values = {},
+      tokens = {},
+      comments = {},
       block = nil,
     }
-    setmetatable(chunk, chunk_mt)
-    table.insert(self.stack, chunk)
-    self.top = chunk
+    setmetatable(directive, chunk_mt)
+    table.insert(self.stack, directive)
+    self.top = directive
     return true
   end,
   
-  popChunk = function(self)
-    assert(self:inChunk())
-    local chunk = self.top
-    chunk.position.last = self.cur
+  popDirective = function(self)
+    assert(self:inDirective())
+    local directive = self.top
+    directive.position.last = self.cur
     
     table.remove(self.stack)
     self.top = self.stack[#self.stack]
     assert(self:inBlock())
-    self.top:addChunk(chunk)
+    self.top:addDirective(directive)
     return true
   end,
   
-  addValueToChunk = function(self, value_type, val)
+  addValueToDirective = function(self, value_type, val)
     if val == nil then
       val = self:match() --last match
     end
@@ -135,21 +165,26 @@ parser_mt = {__index = {
       raw = val
     }
     
-    assert(self:inChunk())
+    assert(self:inDirective())
     self.top:addValue(value)
     return true
   end,
   
   pushBlock = function(self, block_name)
     local block = {
+      type = "block",
       name = block_name,
       position = {
         first = self.cur,
-        last = self.cur
+        last = self.cur,
+        directives_first = nil,
+        directives_last = nil
       },
-      chunks = {}
+      directives = {},
+      comments = {},
+      tokens = {}
     }
-    setmetatable(block, block_mt)
+    setmetatable(block, chunk_mt)
     table.insert(self.stack, block)
     self.top = block
     return true
@@ -159,12 +194,12 @@ parser_mt = {__index = {
     local block = self.top
     table.remove(self.stack)
     self.top = self.stack[#self.stack]
-    assert(self:inChunk())
+    assert(self:inDirective())
     self.top:setBlock(block)
     
-    --finishing a block means we are also finishing the chunk it belongs to
+    --finishing a block means we are also finishing the directive it belongs to
     if block.name ~= "ROOT" then
-      self:popChunk()
+      self:popDirective()
     end
     return true
   end,
@@ -174,6 +209,17 @@ parser_mt = {__index = {
     for _ in self:eachToken() do
       token_count = token_count + 1
     end
+    
+    if self.top.type == "directive" then
+      local err = "unexpected end of file, expected \";\""
+      self:error(err)
+      return nil, err
+    elseif self.top.type == "block" and self.top.name ~= "ROOT" then
+      local err = "unexpected end of file, expected \"}\""
+      self:error(err)
+      return nil, err
+    end
+      
     return self.top
   end,
   
@@ -212,15 +258,31 @@ parser_mt = {__index = {
     end
   end,
   
-  skipSpace = function(self)
-    return self:match("^[ \t\r\n]+", "last_space")
+  skipSpace = function(self, spacechars)
+    spacechars = spacechars or " \t\r\n"
+    local m = self:match("^["..spacechars.."]+", "last_space")
+    if m and self:inDirective() then
+      local nl = nil
+      repeat
+        nl = m:find("\n", nl)
+        if nl then
+          self.top:addNewline(self.cur - #m + nl - 1)
+          nl = nl+1
+        end
+      until not nl
+    end
+    return m
   end,
   skipComment = function(self)
-    return self:match("^#[^\n]+", "last_comment")
+    local m = self:match("^#[^\n]+", "last_comment")
+    if m then
+      self.top:addComment(m, self.cur - #m)
+    end
+    return m
   end,
   
   matchString = function(self)
-    local quote_char = assert(self:match("^['\"]", false))
+    local quote_char = self.str:sub(self.cur, self.cur)
     local unquote
     local offset = 1
     repeat
@@ -229,9 +291,9 @@ parser_mt = {__index = {
         offset = unquote - self.cur + 1
         if countSlashesReverse(self.str, self.cur, unquote - 1) % 2 == 0 then
           --string end found
-          self.last_match = self.str:sub(self.cur+1, unquote)
+          self.last_match = self.str:sub(self.cur, unquote)
           self.cur = unquote + 1
-          return self:addValueToChunk("string")
+          return self:addValueToDirective("string")
         end
       end
     until not unquote
@@ -241,7 +303,7 @@ parser_mt = {__index = {
   matchVariable = function(self)
     local var = self:match("^%$([%w%.%_]+)")
     if var then
-      return self:addValueToChunk("variable")
+      return self:addValueToDirective("variable")
     end
     
     if self:match("^%$[^%s;]+") then
@@ -261,7 +323,32 @@ parser_mt = {__index = {
       self:error("invalid value")
       return nil
     end
-    return self:addValueToChunk("value")
+    return self:addValueToDirective("value")
+  end,
+  
+  matchDirectiveName = function(self)
+    if not self:match("^[%w%._]+") then
+      if self:match("^;") then
+        self:error("unexpected \";\"")
+      elseif self:match("^{") then
+        self:error("unexpected \"{\"")
+      elseif self:match("^%S+") then
+        self:error("invalid config directive name \"" .. self:match() .. "\"" , "^%S+")
+      else
+        self:error("expected config directive")
+      end
+      return nil
+    end
+    self:pushDirective(self:match())
+    return true
+  end,
+  
+  matchSemicolon = function(self)
+    assert(self:match("^;"))
+    self.top:addSemicolon(self.cur - 1)
+    self:skipSpace(" \t")
+    self:skipComment()
+    return self:popDirective()
   end,
   
   printStack = function(self)
@@ -278,31 +365,30 @@ parser_mt = {__index = {
       if self:match("^}") then
         return self:popBlock()
       else
-        local directive_name = self:match("^[%w%._]+")
-        if not directive_name then
-          self:error("expected config directive name", "^%S+")
+        local ok, err = self:matchDirectiveName()
+        if not ok then
+          self:error(err)
           return nil
         end
-        self:pushChunk(directive_name)
       end
       return true
     end
     
-    assert(self:inChunk())
-    local char = self:match("^.", false)
+    assert(self:inDirective())
+    local char = self.str:sub(self.cur, self.cur)
     local ok, err
     if char == "\"" or char == "'" then
-      self.cur = self.cur - 1
       ok, err = self:matchString()
     elseif char == "$" then
-      self.cur = self.cur - 1
       ok, err = self:matchVariable()
     elseif char == "{" then
+      self.cur = self.cur + 1
       ok, err = self:pushBlock()
+    elseif char == "}" then
+      err = "unexpected \"}\""
     elseif char == ";" then
-      ok, err = self:popChunk()
+      ok, err = self:matchSemicolon()
     else
-      self.cur = self.cur - 1
       ok, err = self:matchValue()
     end
     
@@ -316,7 +402,6 @@ parser_mt = {__index = {
   location = function(self, pos)
     local line = 1
     local cur = 1
-    local current_line_start = -1
     pos = pos or self.cur
     if pos < 0 then
       pos = #self.str - pos
@@ -324,17 +409,64 @@ parser_mt = {__index = {
     while true do
       local nl = self.str:find("\n", cur, true)
       if not nl then --no more newlines
+        print "no more newlines"
         break
-      elseif cur <= pos then --newline in-range
+      elseif nl < pos then --newline in-range
         line = line + 1
-        current_line_start = pos
-        cur = pos + 1
+        cur = nl + 1 
       else -- we've gone past last
         break
       end
     end
-    return line, pos - current_line_start + 1
+    return line, pos - cur
   end,
+  
+  debugString = function(self, cur, lvl)
+    lvl = lvl or 0
+    cur = cur or self.top
+    local buf = {}
+    if cur.type == "block" then
+      for _, v in ipairs(cur.tokens) do
+        table.insert(buf, self:debugString(v, lvl))
+      end
+      if cur.name == "ROOT" then
+        return table.concat(buf, "\n")
+      else
+        return ("{\n%s\n%s}\n"):format(table.concat(buf, "\n"), indent(lvl-1))
+      end
+    elseif cur.type == "directive" then
+      local str = indent(lvl) .. cur.name
+      local prev_type = "none"
+      for i, token in ipairs(cur.tokens) do
+        local pre = prev_type == "newline" and indent(lvl+1) or " "
+        
+        if token.type == "newline" then
+          str = str .. "\n"
+        elseif token.type == "comment" then
+          str = str .. pre .. token.value
+        elseif token.type == "value" or token.type == "string" or token.type == "variable" then
+          str = str .. pre .. "<"..token.raw..">"
+        elseif token.type == "semicolon" then
+          str = str .. (prev_type == "newline" and indent(lvl) or "")..";"
+        else
+          error("unexpected token type " .. token.type)
+        end
+        
+        prev_type = token.type
+      end
+      
+      if cur.block then
+        str = str .. " " .. self:debugString(cur.block, lvl + 1)
+        return str
+      else
+        return str
+      end
+    elseif cur.type == "comment" then
+      return indent(lvl)..cur.value
+    else
+      error("unexpected chunk type " .. cur.type)
+    end
+  end
 }}
 
 return Parser
