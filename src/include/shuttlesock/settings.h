71ee52c0747bcd73c6879ab3ab7712071fa7fa6b
Leo P <junk@slact.net>
2019-09-25 19:08:10 -0400

    settings work checkin.. not much here.

--- src/include/shuttlesock/settings.h
+++ src/include/shuttlesock/settings.h
@@ -34,4 +34,14 @@ struct shuso_setting_s {
 
 extern shuso_setting_value_t SHUTTLESOCK_VALUE_END;
 
+typedef void shuso_setting_context_t; //placeholder
+
+//For module developers
+shuso_setting_t *shuso_setting(shuso_t *S, const char *name, shuso_setting_context_t *ctx);
+bool shuso_setting_set_error(shuso_t *S, const char *fmt, ...);
+
+
+//for shuttlesock developers' eyes only
+bool shuso_module_register_setting(shuso_t *S, shuso_module_t *module, shuso_setting_t *setting);
+
 #endif //SHUSO_SETTINGS_H
--- src/settings.c
+++ src/settings.c
@@ -1,6 +1,12 @@
-#include <shuttlesock.h>
 #include <shuttlesock/settings.h>
+#include <shuttlesock.h>
+#include <shuttlesock/lua_bridge.h>
 
 shuso_setting_value_t SHUTTLESOCK_VALUE_END = {
   .type = SHUSO_VALUE_END_SENTINEL,
 };
+
+
+bool shuso_module_register_setting(shuso_t *S, shuso_module_t *module, shuso_setting_t *setting) {
+  return true;
+}
