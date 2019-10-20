a7a26a3a9ffdf832758548d99a40687c3134ea82
Leo P <junk@slact.net>
2019-10-20 00:20:30 -0400

    generate lua script pack during build, not configure

--- cmake/PackLuaScripts.cmake
+++ cmake/PackLuaScripts.cmake
@@ -1,136 +1,116 @@
 include(CMakeParseArguments)
 
-function(pack_lua_scripts_start)
-  set(oneValueArgs SOURCE SCRIPTS_PATH)
-  set(SHUTTLESOCK_LUA_SCRIPT_NAMES "" CACHE INTERNAL "lua script names" FORCE)
-  set(SHUTTLESOCK_LUA_SCRIPT_SCRIPTS "" CACHE INTERNAL "lua scripts" FORCE)
-  set(SHUTTLESOCK_LUA_SCRIPT_IS_MODULE "" CACHE INTERNAL "lua script as a module" FORCE)
-  cmake_parse_arguments(LUA_SCRIPTS "" "${oneValueArgs}" "" ${ARGN})
-  set(SHUTTLESOCK_LUA_SCRIPTS_SOURCE "${LUA_SCRIPTS_SOURCE}" CACHE INTERNAL "lua scripts source file" FORCE)
-  set(SHUTTLESOCK_LUA_SCRIPTS_PATH "${LUA_SCRIPTS_SCRIPTS_PATH}" CACHE INTERNAL "lua scripts path" FORCE)
-  if(NOT SHUTTLESOCK_NO_LUAC)
-    find_program(LUAC_PROGRAM NAMES luac53 luac5.3 luac)
+find_program(LUA_PROGRAM NAMES lua53 lua5.3 lua)
+if(NOT LUA_PROGRAM)
+  message(FATAL_ERROR "Could not find Lua program")
+endif()
+if(LUA_PROGRAM)
+  execute_process(
+    COMMAND "${LUA_PROGRAM}" -v
+    OUTPUT_VARIABLE lua_output
+  )
+  string(FIND "${lua_output}" "Lua ${LUA_VERSION_STRING}" lua_version_match)
+  if("${lua_version_match}" EQUAL "-1")
+    message(FATAL_ERROR "Wrong Lua program version, expected ${LUA_VERSION_STRING}, got ${lua_output}")
   endif()
-  if(LUAC_PROGRAM)
-    execute_process(
-      COMMAND "${LUAC_PROGRAM}" -v
-      OUTPUT_VARIABLE luac_output
-    )
-    string(FIND "${luac_output}" "Lua ${LUA_VERSION_STRING}" luac_version_match)
-    if("${luac_version_match}" EQUAL "-1")
-      set(LUAC_PROGRAM "" CACHE INTERNAL "luac path" FORCE)
-    endif()
-  endif()
-  
-  find_program(LUACHECK_PROGRAM NAMES luacheck)
-  
-endfunction()
+endif()
 
-function(pack_lua_scripts_add)
-  set(options MODULE CHECK)
-  set(oneValueArgs NAME SCRIPT)
-  cmake_parse_arguments(LUA_SCRIPTS "${options}" "${oneValueArgs}" "" ${ARGN})
-  list(APPEND SHUTTLESOCK_LUA_SCRIPT_NAMES ${LUA_SCRIPTS_NAME})
-  list(APPEND SHUTTLESOCK_LUA_SCRIPT_FILES ${LUA_SCRIPTS_SCRIPT})
-  if("${LUA_SCRIPTS_MODULE}")
-    list(APPEND SHUTTLESOCK_LUA_SCRIPT_IS_MODULE "true")
+if(NOT DEFINED HAVE_LUACHECK)
+  message(STATUS "Check if luacheck is installed ")
+  execute_process(
+    COMMAND "${LUA_PROGRAM}" "PackLuaScripts.lua" "--check"
+    RESULT_VARIABLE luacheck_res
+    OUTPUT_VARIABLE luacheck_out
+    ERROR_VARIABLE luacheck_err
+    WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}"
+  )
+  if("${luacheck_res}" EQUAL 0)
+    message(STATUS "Check if luacheck is installed - yes")
+    set(HAVE_LUACHECK "yes" CACHE INTERNAL "is the luacheck luarock installed?")
   else()
-    list(APPEND SHUTTLESOCK_LUA_SCRIPT_IS_MODULE "false")
+    message(STATUS "Check if luacheck is installed - no. Lua scripts will not be checked. ")
+    set(HAVE_LUACHECK "no" CACHE INTERNAL "is the luacheck luarock installed?")
   endif()
-  set(SHUTTLESOCK_LUA_SCRIPT_NAMES "${SHUTTLESOCK_LUA_SCRIPT_NAMES}" CACHE INTERNAL "lua script names" FORCE)
-  set(SHUTTLESOCK_LUA_SCRIPT_FILES "${SHUTTLESOCK_LUA_SCRIPT_FILES}" CACHE INTERNAL "lua script files" FORCE)
-  set(SHUTTLESOCK_LUA_SCRIPT_IS_MODULE "${SHUTTLESOCK_LUA_SCRIPT_IS_MODULE}" CACHE INTERNAL "lua script as a module" FORCE)
-  if(LUA_SCRIPTS_CHECK)
-    message(STATUS "Check Lua script ${LUA_SCRIPTS_SCRIPT}")
-    if(LUACHECK_PROGRAM)
-      execute_process(
-        COMMAND "${LUACHECK_PROGRAM}" "${LUA_SCRIPTS_SCRIPT}"
-        RESULT_VARIABLE luacheck_res
-        OUTPUT_VARIABLE luacheck_out
-        ERROR_VARIABLE luacheck_err
-        WORKING_DIRECTORY "${SHUTTLESOCK_LUA_SCRIPTS_PATH}"
-      )
-      if(NOT("${luacheck_res}" EQUAL 0))
-        message(FATAL_ERROR "Check Lua script ${LUA_SCRIPTS_SCRIPT} - failed:\n ${luacheck_out} ${luacheck_err}")
-      endif()
-      message(STATUS "Check Lua script ${LUA_SCRIPTS_SCRIPT} - ok")
-    else()
-      message(STATUS "Check Lua script ${LUA_SCRIPTS_SCRIPT} - skipped (no luacheck found)")
-    endif()
-  endif()
-endfunction()
+endif()
 
-function(pack_lua_scripts_finish)
-  list(LENGTH SHUTTLESOCK_LUA_SCRIPT_NAMES SCRIPT_COUNT)
+if(NOT SHUTTLESOCK_NO_LUAC)
+  find_program(LUAC_PROGRAM NAMES luac53 luac5.3 luac)
+endif()
+if(LUAC_PROGRAM)
+  execute_process(
+    COMMAND "${LUAC_PROGRAM}" -v
+    OUTPUT_VARIABLE luac_output
+  )
+  string(FIND "${luac_output}" "Lua ${LUA_VERSION_STRING}" luac_version_match)
+  if("${luac_version_match}" EQUAL "-1")
+    set(LUAC_PROGRAM "" CACHE INTERNAL "luac path" FORCE)
+  endif()
+endif()
+
+macro(add_lua_script module_name source_file)
+  set(options MODULE CHECK BUNDLED)
+  cmake_parse_arguments(LUA_SCRIPT "${options}" "" "" ${ARGN})
+  set(outfile "${source_file}.out")
+  if(LUA_SCRIPT_MODULE)
+    set(script_kind "module")
+    set(is_module "true")
+  else()
+    set(script_kind "script")
+    set(is_module "false")
+  endif()
   
-  set(SHUTTLESOCK_LUA_SCRIPTS_COMPILED "${LUAC_PROGRAM}" CACHE INTERNAL "compiled lua scripts" FORCE)
+  if(LUA_SCRIPT_BUNDLED)
+    set(LUA_SCRIPT_CHECK "")
+    set(is_bundled " bundled")
+  endif()
   
-  foreach(SCRIPT_NAME IN ITEMS ${SHUTTLESOCK_LUA_SCRIPT_NAMES})
-    list(FIND SHUTTLESOCK_LUA_SCRIPT_NAMES "${SCRIPT_NAME}" SCRIPT_N)
-    list(GET SHUTTLESOCK_LUA_SCRIPT_FILES ${SCRIPT_N} SCRIPT_FILE)
-    
-    list(GET SHUTTLESOCK_LUA_SCRIPT_FILES ${SCRIPT_N} SCRIPT_FILE)
-    list(GET SHUTTLESOCK_LUA_SCRIPT_IS_MODULE ${SCRIPT_N} SCRIPT_IS_MODULE)
-    
-    if(LUAC_PROGRAM)
-      set(luac_outfile ${PROJECT_BINARY_DIR}/pack_lua_scripts/${SCRIPT_FILE}.luac.out)
-      file(WRITE "${luac_outfile}" "")
-      message(STATUS "Compiling Lua script ${SCRIPT_FILE}")
-      execute_process(
-        COMMAND "${LUAC_PROGRAM}" -o "${luac_outfile}" "${SHUTTLESOCK_LUA_SCRIPTS_PATH}/${SCRIPT_FILE}"
-        RESULT_VARIABLE luac_res
-        OUTPUT_VARIABLE luac_out
-        ERROR_VARIABLE luac_err
-      )
-      if(NOT("${luac_res}" EQUAL 0))
-        message(FATAL_ERROR "Failed to compile Lua script ${SCRIPT_FILE}: ${luac_err}")
-      endif()
-      
-      file(READ "${luac_outfile}" SCRIPT_STRING HEX)
-      
-      string(LENGTH "${SCRIPT_STRING}" SCRIPT_STRING_LENGTH)
-      math(EXPR SCRIPT_STRING_LENGTH "${SCRIPT_STRING_LENGTH} / 2")
-      
-      string(REGEX REPLACE "([0-9a-f][0-9a-f])" "\\\\x\\1" SCRIPT_STRING ${SCRIPT_STRING})
-      set(script_key "compiled")
-      set(script_len_key "compiled_len")
-      message(STATUS "Compiling Lua script ${SCRIPT_FILE} - ok")
-    else()
-      message(STATUS "Including Lua script ${SCRIPT_FILE}")
-      # reads source file contents as text string
-      file(READ "${SHUTTLESOCK_LUA_SCRIPTS_PATH}/${SCRIPT_FILE}" SCRIPT_STRING)
-      string(LENGTH "${SCRIPT_STRING}" SCRIPT_STRING_LENGTH)
-      
-      #escape quotes and newlines, and indent the newlines
-      string(REGEX REPLACE "\\\\" "\\\\\\\\" SCRIPT_STRING "${SCRIPT_STRING}")
-      string(CONFIGURE "\${SCRIPT_STRING}" SCRIPT_STRING ESCAPE_QUOTES)
-      string(REGEX REPLACE "\n" "\\\\n\"\n      \"" SCRIPT_STRING "${SCRIPT_STRING}")
-      
-      set(script_key "script")
-      set(script_len_key "script_len")
-      message(STATUS "Including Lua script ${SCRIPT_FILE} - ok")
-    endif()
-    
-    string(APPEND SCRIPTS_STRUCT
-      "  {\n"
-      "    .name = \"${SCRIPT_NAME}\",\n"
-      "    .module = ${SCRIPT_IS_MODULE},\n"
-      "    .filename = \"${SHUTTLESOCK_LUA_SCRIPTS_PATH}/${SCRIPT_FILE}\",\n"
-      "    .${script_key} = \"${SCRIPT_STRING}\",\n"
-      "    .${script_len_key} = ${SCRIPT_STRING_LENGTH}\n"
-      "  },\n"
-    )
-  endforeach()
-  
-  file(REMOVE_RECURSE "${PROJECT_BINARY_DIR}/pack_lua_scripts/")
-  
-  file(WRITE "${PROJECT_BINARY_DIR}/src/${SHUTTLESOCK_LUA_SCRIPTS_SOURCE}"
-    "//auto-generated original content, pls don't steal\n"
-    "#include <shuttlesock/embedded_lua_scripts.h>\n"
-    "shuso_lua_embedded_scripts_t shuttlesock_lua_embedded_scripts[] = {\n"
-    "${SCRIPTS_STRUCT}\n"
-    "  {.name=NULL,.script=NULL}\n"
-    "};\n"
+  if("${LUA_SCRIPT_CHECK}" AND "${HAVE_LUACHECK}")
+    set(checkarg "--check")
+    set(command_check " and checking")
+  else()
+    set(checkarg "--nocheck")
+    set(command_check "")
+  endif()
+  get_filename_component(outfile_dir ${outfile} DIRECTORY)
+  file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${outfile_dir})
+  if(LUAC_PROGRAM)
+    set(command_action "Compiling")
+    set(compile_command ${LUAC_PROGRAM} -o ${CMAKE_CURRENT_BINARY_DIR}/${outfile} ${source_file})
+    set(source_compiled "true")
+  else()
+    set(command_action "Including")
+    set(compile_command ${CMAKE_COMMAND} -E copy "${source_file}" "${CMAKE_CURRENT_BINARY_DIR}/${outfile}")
+    set(source_compiled "false")
+  endif()
+  add_custom_command(OUTPUT ${outfile}
+    COMMAND ${LUA_PROGRAM} cmake/PackLuaScripts.lua ${checkarg} "${source_file}"
+    COMMAND ${compile_command}
+    DEPENDS ${source_file}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+    COMMENT "${command_action}${command_check}${is_bundled} Lua ${script_kind} \"${module_name}\" ${source_file}"
   )
   
+  set(packed_lua_scripts_target_deps ${packed_lua_scripts_target_deps} ${outfile} PARENT_SCOPE)
+  set(packed_lua_scripts_config "${packed_lua_scripts_config} {src='${source_file}', name='${module_name}', file='${CMAKE_CURRENT_BINARY_DIR}/${outfile}', module=${is_module}, compiled=${source_compiled}}," PARENT_SCOPE)
+endmacro()
+
+
+function(add_lua_module module_name source_file)
+  add_lua_script(${module_name} ${source_file} MODULE CHECK)
+endfunction()
+function(add_lua_bundled_module module_name source_file)
+  add_lua_script(${module_name} ${source_file} MODULE BUNDLED)
+endfunction()
+
+function(set_lua_packed_script_file packed_filename)
+  set(lua_packer_config_file "${CMAKE_CURRENT_BINARY_DIR}/src/lua_packer_config.lua")
+  file(WRITE ${lua_packer_config_file}
+    "return {${packed_lua_scripts_config}}"
+  )
+
+  add_custom_command(OUTPUT ${packed_filename}
+    COMMAND ${LUA_PROGRAM} ${CMAKE_CURRENT_LIST_DIR}/cmake/PackLuaScripts.lua --pack ${lua_packer_config_file} ${CMAKE_CURRENT_BINARY_DIR}/${packed_filename}
+    DEPENDS ${packed_lua_scripts_target_deps} ${lua_packer_config_file}
+  )
+
 endfunction()
---
+++ cmake/PackLuaScripts.lua
@@ -0,0 +1,108 @@
+local mm = require "mm"
+
+local function fail(str, ...)
+  local arg= {...}
+  if #arg == 0 then
+    io.stderr:write(str.."\n")
+  else
+    io.stderr:write(str:format(...).."\n")
+  end
+  os.exit(1)
+end
+
+local ARG={...}
+
+if ARG[1] == "--nocheck" then
+  --do nothing. this is here just to make the CMake script less repetitive
+  return
+elseif ARG[1] == "--check" then
+  local ok, luacheck  = pcall(require, "luacheck")
+  if not ok then
+    fail("luacheck not found")
+  end
+  local luacheck_config = require "luacheck.config"
+  if #ARG == 1 then return end
+  
+  local conf = luacheck_config.load_config()
+  local files = {}
+  for i=2, #ARG do
+    table.insert(files, ARG[i])
+  end
+  
+  local report = luacheck(files, conf.options)
+  if report.errors == 0 and report.warnings == 0 and report.fatals == 0 then
+    return
+  end
+  
+  local out = require "luacheck.format".format(report, files, {quiet=1})
+  io.stderr:write(out.."\n\n")
+  os.exit(1)
+elseif ARG[1] == "--pack" then
+  local scripts = dofile(ARG[2])
+
+  local struct_fmt = [[  {
+      .name = "%s",
+      .module = %s,
+      .filename = "%s",
+      .%s = "%s",
+      .%s = %d
+    }]]
+
+  local packed_fmt =[[//auto-generated original content, pls don't steal or edit
+  #include <shuttlesock/embedded_lua_scripts.h>
+  shuso_lua_embedded_scripts_t shuttlesock_lua_embedded_scripts[] = {
+  %s
+  };
+  ]]
+
+  local struct = {}
+
+  for _, script in ipairs(scripts) do
+    local path = script.file or script.src
+    local file = io.open(path, "rb")
+    if not file then
+      fail("failed to open file %s", path)
+    end
+    script.data=file:read("*a")
+    file:close()
+    local out
+    if script.compiled then
+      out = script.data:gsub(".", function(c)
+        return ("\\x%02x"):format(c:byte())
+        --local byte = c:byte()
+        --if byte < 0x30 or byte == 0x22 or byte > 0x7E then
+        --  return ("\\x%02x"):format(c:byte())
+        --end
+      end)
+    else
+      out = script.data
+      out = out:gsub('\\', '\\\\')
+      out = out:gsub('"', '\\"')
+      out = out:gsub("\r?\n", function(c)
+        if c == "\r\n" then
+          return '\\r\\n"\n      "'
+        else
+          return '\\n"\n      "'
+        end
+      end)
+    end
+    
+    script.struct = struct_fmt:format(script.name, script.module and "true" or "false", script.src, script.compiled and "compiled" or "script", out, script.compiled and "compiled_len" or "script_len", #script.data)
+    
+    table.insert(struct, script.struct)
+  end
+
+  table.insert(struct, "  {.name=NULL,.script=NULL}")
+
+  local out = packed_fmt:format(table.concat(struct, ",\n"))
+  local outpath = ARG[3]
+  local outfile = io.open(outpath, "wb")
+  if not outfile then
+    fail("failed to open output file %s", outpath)
+  end
+
+  outfile:write(out)
+  outfile:close()
+else
+  fail("first arg must be --pack or --check")
+end
