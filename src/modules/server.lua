3c11fc9e5e6f67238469edce83c583cb22cf44c6
Leo P <junk@slact.net>
2019-12-02 07:41:36 -0500

    more server module skellies

--- src/modules/server.lua
+++ src/modules/server.lua
@@ -1,1 +1,31 @@
-return {name="server"}
+local Module = require "shuttlesock.module"
+
+-- luacheck: ignore CFuncs
+local CFuncs = require "shuttlesock.modules.core.server.cfuncs"
+
+local Server = Module.new {
+  name = "server",
+  version = require "shuttlesock".VERSION,
+  publish = {
+    listen_token= {data_type="string"}
+  }
+}
+
+Server.settings = {
+  {
+    name = "server",
+    path = "(http|stream)/",
+    description = "Configuration block for a server.",
+    nargs = 0,
+    block = true
+  },
+  {
+    name = "listen",
+    path = "server/",
+    description = "Sets the address and port for the socket on which the server will accept connections. It is possible to specify just the port. The address can also be a hostname.",
+    default_value = "$default_listen_host:$default_listen_port",
+    nargs = "1-32",
+  }
+}
+
+return Server
--- src/modules/server/server.c
+++ src/modules/server/server.c
@@ -1,6 +1,18 @@
 #include <shuttlesock.h>
+#include <lualib.h>
+#include <lauxlib.h>
 #include "server.h"
 
+static int server_foo(lua_State *L) {
+  return 0;
+}
+
+
 void shuttlesock_server_module_prepare(shuso_t *S, void *pd) {
-  luaS_do_embedded_script(S->lua.state, "shuttlesock_server_module_setup", 0);
+  luaL_Reg lib[] = {
+    {"foo", server_foo},
+    {NULL, NULL}
+  };
+  
+  luaS_register_lib(S->lua.state, "shuttlesock.modules.core.server.cfuncs", lib);
 }
