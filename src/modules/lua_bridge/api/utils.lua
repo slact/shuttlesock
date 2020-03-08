fed6f3f5f6ab941abb21e5b53351fe5541b8e7e3
Leo P <junk@slact.net>
2020-03-08 06:32:10 -0400

    has_root eh?

--- src/modules/lua_bridge/api/utils.lua
+++ src/modules/lua_bridge/api/utils.lua
@@ -1,7 +1,6 @@
 local Core = require "shuttlesock.core"
 local Utils = {}
 
-Utils.parse_hosts = Core.parse_hosts
-Utils.master_has_superuser = Core.master_has_superuser
+Utils.master_has_root = Core.master_has_root
 
 return Utils
